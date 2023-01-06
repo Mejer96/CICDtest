cf6330c3f0c9c875d3e44f5afef12d146ca31b59
mejer96 <carlemilfalkmejer96@gmail.com>
2023-01-06 05:32:19 +0100

    commit 1

--- src/main/java/com/example/cicdtest/Controller.java
+++ src/main/java/com/example/cicdtest/Controller.java
@@ -2,13 +2,14 @@ package com.example.cicdtest;
 
 import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
 
     @GetMapping("/")
     public String getIndex() {
-        return "index";
+        return "hello world";
     }
 
 }
